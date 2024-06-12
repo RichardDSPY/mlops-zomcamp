796029c276ea2e29d9212fc051f0f437299d2143
Richard Ramos Flores <richard.ds.py@gmail.com>
2024-06-11 21:48:27 -0500

    avance homework 3 - mage

---
+++ mlops/mlops/homework_03/transformers/prepare.py
@@ -0,0 +1,32 @@
+import pandas as pd
+
+from mlops.homework_03.utils.cleaning import clean
+from mlops.homework_03.utils.feature_selector import select_features
+from mlops.homework_03.utils.splitters import split_on_value
+
+if 'transformer' not in globals():
+    from mage_ai.data_preparation.decorators import transformer
+
+
+@transformer
+def transform(
+    df: pd.DataFrame, **kwargs
+) -> pd.DataFrame:
+
+    split_on_feature = kwargs.get('split_on_feature', 'tpep_pickup_datetime')
+    split_on_feature_value = kwargs.get('split_on_feature_value', '2023-04-01')
+    target = kwargs.get('target', 'duration')
+
+    df = clean(df)
+    df = select_features(df, features=[split_on_feature, target])
+
+    df_train, df_val = split_on_value(
+        df,
+        split_on_feature,
+        split_on_feature_value
+    )
+
+    return df, df_train, df_val
+
+
+
